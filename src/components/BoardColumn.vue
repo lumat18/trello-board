4e0f637b7db921bf25a3610d9c7e6d452f03ea9c
lumat18 <lumat@op.pl>
2020-08-30 23:32:50 +0200

    extract MovingTasksAndColumnsMixin

--- src/components/BoardColumn.vue
+++ src/components/BoardColumn.vue
@@ -27,24 +27,12 @@
 
 <script>
 import ColumnTask from "./ColumnTask";
+import MovingTasksAndColumnsMixin from "../mixins/MovingTasksAndColumnsMixin";
 export default {
   name: "BoardColumn",
+  mixins: [MovingTasksAndColumnsMixin],
   components: {
     ColumnTask
-  },
-  props: {
-    column: {
-      type: Object,
-      required: true
-    },
-    columnIndex: {
-      type: Number,
-      required: true
-    },
-    board: {
-      type: Object,
-      required: true
-    }
   },
   methods: {
     createTask(event, tasks) {
@@ -59,30 +47,6 @@ export default {
       event.dataTransfer.dropEffect = "move";
       event.dataTransfer.setData("from-column-index", fromColumnIndex);
       event.dataTransfer.setData("type", "column");
-    },
-    moveTaskOrColumn(event, toTasks, toColumnIndex, toTaskIndex) {
-      const type = event.dataTransfer.getData("type");
-      if (type === "task") {
-        this.moveTask(event, toTasks, toTaskIndex);
-      } else {
-        this.moveColumn(event, toColumnIndex);
-      }
-    },
-    moveTask(event, toTasks, toTaskIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-      const fromTasks = this.board.columns[fromColumnIndex].tasks;
-      const taskIndex = event.dataTransfer.getData("task-index");
-
-      this.$store.commit("MOVE_TASK", {
-        fromTasks,
-        toTasks,
-        taskIndex,
-        toTaskIndex
-      });
-    },
-    moveColumn(event, toColumnIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-      this.$store.commit("MOVE_COLUMN", { fromColumnIndex, toColumnIndex });
     }
   }
 };
--- src/components/ColumnTask.vue
+++ src/components/ColumnTask.vue
@@ -13,8 +13,10 @@
   </div>
 </template>
 <script>
+import MovingTasksAndColumnsMixin from "../mixins/MovingTasksAndColumnsMixin";
 export default {
   name: "ColumnTask",
+  mixins: [MovingTasksAndColumnsMixin],
   props: {
     task: {
       type: Object,
@@ -22,18 +24,6 @@ export default {
     },
     taskIndex: {
       type: Number,
-      required: true
-    },
-    column: {
-      type: Object,
-      required: true
-    },
-    columnIndex: {
-      type: Number,
-      required: true
-    },
-    board: {
-      type: Object,
       required: true
     }
   },
@@ -47,30 +37,6 @@ export default {
       event.dataTransfer.setData("task-index", taskIndex);
       event.dataTransfer.setData("from-column-index", fromColumnIndex);
       event.dataTransfer.setData("type", "task");
-    },
-    moveTaskOrColumn(event, toTasks, toColumnIndex, toTaskIndex) {
-      const type = event.dataTransfer.getData("type");
-      if (type === "task") {
-        this.moveTask(event, toTasks, toTaskIndex);
-      } else {
-        this.moveColumn(event, toColumnIndex);
-      }
-    },
-    moveTask(event, toTasks, toTaskIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-      const fromTasks = this.board.columns[fromColumnIndex].tasks;
-      const taskIndex = event.dataTransfer.getData("task-index");
-
-      this.$store.commit("MOVE_TASK", {
-        fromTasks,
-        toTasks,
-        taskIndex,
-        toTaskIndex
-      });
-    },
-    moveColumn(event, toColumnIndex) {
-      const fromColumnIndex = event.dataTransfer.getData("from-column-index");
-      this.$store.commit("MOVE_COLUMN", { fromColumnIndex, toColumnIndex });
     }
   }
 };
