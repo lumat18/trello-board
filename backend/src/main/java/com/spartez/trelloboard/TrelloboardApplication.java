b945efd179fe9d13e64c6f6ae467eab0592a2596
lumat18 <lumat@op.pl>
2020-09-04 00:45:36 +0200

    divide into frontend/backend maven modules

---
+++ backend/src/main/java/com/spartez/trelloboard/TrelloboardApplication.java
@@ -0,0 +1,13 @@
+package com.spartez.trelloboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloboardApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/spartez/trelloboard/TrelloboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spartez.trelloboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
